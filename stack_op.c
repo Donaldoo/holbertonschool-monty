1c10a33235ce01cf49af93d8738559aae75e443b
Donaldoo <donald.kumaraku@gmail.com>
2022-08-17 02:24:23 -0700

    Added task

--- stack_op.c
+++ stack_op.c
@@ -45,3 +45,23 @@ void push(stack_t **stack, unsigned int line_number, char *num)
 	}
 	*stack = new;
 }
+
+/**
+ * pop - removes the top element of the stack
+ * @stack: pointer to stack
+ * @line_number: line number
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = (*stack)->next;
+	free(*stack);
+	*stack = tmp;
+}
