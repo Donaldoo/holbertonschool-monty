0b13c43148b1cf28ac232f5b15e852650dd06001
Donaldoo <donald.kumaraku@gmail.com>
2022-08-17 03:19:19 -0700

    Added task

---
+++ calculate.c
@@ -0,0 +1,128 @@
+#include "monty.h"
+
+/**
+ * add_ - adds the last 2 elements of the stack
+ * @stack: pointer to stack
+ * @line_number: line number
+ */
+void add_(stack_t **stack, unsigned int line_number)
+{
+	int a, b, result;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	a = (*stack)->n;
+	b = (*stack)->next->n;
+
+	result = a + b;
+
+	pop(stack, line_number);
+
+	(*stack)->n = result;
+}
+
+
+/**
+ * sub_ - substracts the top element of the stack from the second top element
+ * @stack: stack
+ * @line_number: line number
+ */
+void sub_(stack_t **stack, unsigned int line_number)
+{
+	int a, b, result;
+	
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	
+	a = (*stack)->n;
+	b = (*stack)->next->n;
+
+	result = b - a;
+	
+	pop(stack, line_number);
+	
+	(*stack)->n = result;
+}
+
+
+/**
+ * div_ - substracts the top element of the stack from the second top element
+ * @stack: stack
+ * @line_number: line number
+ */
+void div_(stack_t **stack, unsigned int line_number)
+{
+        int a, b, result;
+
+        if (stack == NULL || *stack == NULL)
+        {
+                fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+
+        a = (*stack)->n;
+        b = (*stack)->next->n;
+
+        result = b / a;
+
+        pop(stack, line_number);
+
+        (*stack)->n = result;
+}
+
+/**
+ * mul_ - substracts the top element of the stack from the second top element
+ * @stack: stack
+ * @line_number: line number
+ */
+void mul_(stack_t **stack, unsigned int line_number)
+{
+        int a, b, result;
+
+        if (stack == NULL || *stack == NULL)
+        {
+                fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+
+        a = (*stack)->n;
+        b = (*stack)->next->n;
+
+        result = b * a;
+
+        pop(stack, line_number);
+
+        (*stack)->n = result;
+}
+
+/**
+ * mod_ - substracts the top element of the stack from the second top element
+ * @stack: stack
+ * @line_number: line number
+ */
+void mod_(stack_t **stack, unsigned int line_number)
+{
+        int a, b, result;
+
+        if (stack == NULL || *stack == NULL)
+        {
+                fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+
+        a = (*stack)->n;
+        b = (*stack)->next->n;
+
+        result = b % a;
+
+        pop(stack, line_number);
+
+        (*stack)->n = result;
+}
--- opcode_struct.c
+++ opcode_struct.c
@@ -11,7 +11,8 @@ int get_op_code(char *token, stack_t **stack, unsigned int line_number)
 
 	instruction_t operation[] = {
 		{"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap},
-		{NULL, NULL}
+		{"nop", nop}, {"add", add_}, {"sub", sub_}, {"div", div_},
+		{"mul", mul_}, {"mod", mod_}, {NULL, NULL}
 	};
 
 	for (i = 0; operation[i].opcode != NULL; i++)
